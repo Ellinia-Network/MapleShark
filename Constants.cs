cee9690a6dbdc2cddeb364a49e90dc6421be462a
shailist <shailist2000@gmail.com>
2023-06-02 17:31:28 +0300

    stuff

--- Constants.cs
+++ Constants.cs
@@ -9,10 +9,13 @@ namespace MapleShark
     public static class Constants
     {
 
-        //public static string OpcodeTableKey => "M@PleStoryMaPLe!";
-        public static short OpcodeEncryption = 44;
-        public static short StartClientOp = 200;
-        public static short EndClientOp = 0x680;
-        public static string OpcodeEncryptionKey = "N3x@nGLEUH@ckEr!";
+        //public static short OpcodeEncryption = 44;
+        public static short OpcodeEncryption = 45; // v212.2
+        //public static short StartClientOp = 200;
+        public static short StartClientOp = 207; // v212.2
+        //public static short EndClientOp = 0x680;
+        public static short EndClientOp = 0x761;
+        //public static string OpcodeEncryptionKey = "N3x@nGLEUH@ckEr!";
+        public static string OpcodeEncryptionKey => "M@PleStoryMaPLe!";
     }
 }
